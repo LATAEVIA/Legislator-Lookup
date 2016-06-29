30459b9e40f1201e0a2df4cfde819d907f595ec8
LaTaevia Berry <lataevia.berry@gmail.com>
2016-06-29 15:58:27 -0700

    create a model hook on results route using those same parameters from the URL to create a GET request
    The getJSON method runs asynchronously and returns what's called a
    promise. A promise is a standard way of dealing with the results of
    asynchronous operations. When getJSON receives a response from the
    server, it invokes the callback function passed to .then(), and the
    JSON from the response is assigned to the parameter, which we've called
    responseJSON. Now, in the callback function, we can add the code that
    should run after the response is received.

--- app/routes/results.js
+++ app/routes/results.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function(params) {
+    var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey=1dfcfb28466c4e10abcf5c947b9ae833&zip=' + params.zip;
+    return Ember.$.getJSON(url).then(function(responseJSON) {
+      return responseJSON.results;
+    });
+  }
 });
